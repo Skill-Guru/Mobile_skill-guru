6fa44715e0e2243d6912f1dd9a2ad1b63359f1dc
AlexandreDujardin <alexandre.dujardin14@orange.fr>
2024-09-27 10:06:33 +0200

    remove files

--- lib/models/product.dart
+++
@@ -1,8 +0,0 @@
-class Product {
-  String name;
-  String imageUrl;
-  String description;
-  double price;
-
-  Product({required this.name, required this.imageUrl, required this.description, required this.price});
-}
