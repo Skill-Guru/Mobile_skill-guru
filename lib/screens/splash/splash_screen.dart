292e11b29d4b5d3cbd2ec840627edb862462d6ee
SOREAU BASTIEN <80333079+babasssss@users.noreply.github.com>
2024-10-08 10:21:42 +0200

    fix(splah_screen) : add plash_screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -19,33 +19,31 @@ class _SplashScreenState extends State<SplashScreen>
   void initState() {
     super.initState();
 
-    // Configurer l'animation avec une durée totale de 2.5 secondes
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 4, milliseconds: 500), // Durée totale de 2.5 secondes
+      duration: const Duration(seconds: 4), 
     );
 
-    // Scale animation (première partie, 1.25 seconde)
+    // Scale animation
     _scaleAnimation = CurvedAnimation(
       parent: _controller,
-      curve: const Interval(0.0, 0.5, curve: Curves.easeInOut), // 50% du temps = 1.25s
+      curve: const Interval(0.0, 0.5, curve: Curves.easeInOut),
     );
 
-    // Slide animation (deuxième partie, 1.25 seconde)
+    // Slide animation
     _slideAnimation = Tween<Offset>(
       begin: Offset.zero,
-      end: const Offset(1.5, 0.0), // Le logo glisse vers la droite
+      end: const Offset(3, 0.0),
     ).animate(
       CurvedAnimation(
         parent: _controller,
-        curve: const Interval(0.5, 1.0, curve: Curves.elasticIn), // 50% du temps = 1.25s
+        curve: const Interval(0.5, 1.0, curve: Curves.elasticIn),
       ),
     );
 
     // Lancer l'animation
     _controller.forward();
 
-    // Naviguer vers HomeScreen après 2.5 secondes
     Timer(const Duration(seconds: 5), () {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => HomeScreen()),
@@ -62,14 +60,14 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorConstants.primaryColor, // Même couleur que ton splash
+      backgroundColor: ColorConstants.primaryColor,
       body: Center(
         child: ScaleTransition(
           scale: _scaleAnimation,
           child: SlideTransition(
             position: _slideAnimation,
             child: Image.asset(
-              'assets/images/logo.png', // Ton logo
+              'assets/images/logo-removebg.png',
               width: 150,
               height: 150,
             ),
