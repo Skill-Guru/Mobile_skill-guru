cf0bd8908ef3285eea67110e279910bc9a07a27c
SOREAU BASTIEN <80333079+babasssss@users.noreply.github.com>
2024-10-09 14:19:33 +0200

    fix(bastien) : add timer by timerquestion

--- lib/models/quiz_model.dart
+++ lib/models/quiz_model.dart
@@ -2,15 +2,27 @@ class Quiz {
   final String id;
   final String title;
   final List<Question> questions;
+  final int totalTimer;
 
-  Quiz({required this.id, required this.title, required this.questions});
+  Quiz({
+    required this.id, 
+    required this.title, 
+    required this.questions,
+    required this.totalTimer
+    });
 
   factory Quiz.fromJson(Map<String, dynamic> json) {
+
+    int totalTimer = (json['questions'] as List)
+    .fold(0, (total, question) => total + (question['timer'] as int));
+
     return Quiz(
       id: json['id'],
       title: json['title'],
       questions: (json['questions'] as List).map((q) => Question.fromJson(q)).toList(),
+      totalTimer: totalTimer,
     );
+    
   }
 }
 
@@ -18,14 +30,16 @@ class Question {
   final String question;
   final List<String> options;
   final String answer;
-
-  Question({required this.question, required this.options, required this.answer});
+  final int timer;
+  Question({required this.question, required this.options, required this.answer, required this.timer});
 
   factory Question.fromJson(Map<String, dynamic> json) {
     return Question(
       question: json['question'],
       options: List<String>.from(json['options']),
       answer: json['answer'],
+      timer: json['timer'],
     );
   }
 }
+
