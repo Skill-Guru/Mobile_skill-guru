05f934a8e057260c334de6fa122393f83ff8489e
SOREAU BASTIEN <80333079+babasssss@users.noreply.github.com>
2024-10-09 15:46:06 +0200

    fix(dev) : progress bar

--- lib/widget/quiz_widget.dart
+++ lib/widget/quiz_widget.dart
@@ -25,9 +25,26 @@ class QuizWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Calculer la progression en fonction de l'index de la question actuelle
+    double progress = (currentQuestionIndex) / quizzes[0].questions.length;
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
+
+        // Barre de progression
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: LinearProgressIndicator(
+            value: progress, // Affiche la progression
+            backgroundColor: Colors.grey.shade300, // Couleur d'arrière-plan
+            color: ColorConstants.primaryColor, // Couleur de progression
+            minHeight: 8.0, // Hauteur de la barre
+          ),
+        ),
+        SizedBox(height: 20), // Espacement sous la barre de progression
+
+        // Timer widget
         TimerWidget(
           initialTime: quizzes[0].totalTimer,
           onTimeUp: onTimeUp, // Que faire quand le temps est écoulé
@@ -35,14 +52,32 @@ class QuizWidget extends StatelessWidget {
         SizedBox(height: 20),
 
         // Afficher la question en fonction de l'index courant
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center, // Aligne le texte et le trait à gauche
+          children: [
+            Text(
+              'QUESTION ${currentQuestionIndex + 1} :', // Affiche l'index de la question actuelle
+              style: TextStyle(
+                fontSize: 18,
+                color: ColorConstants.secondaryColor, // Couleur du texte
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0), // Espace entre le texte et le trait
+            Divider(
+              color: ColorConstants.primaryColor, // Couleur du trait
+              thickness: 2.0, // Épaisseur du trait
+              endIndent: 20.0, // Décalage à droite du trait si tu veux un trait plus court
+            ),
+          ],
+        ),
         Text(
-          quizzes[0].questions[currentQuestionIndex].question,
+          '${quizzes[0].questions[currentQuestionIndex].question}',
           style: TextStyle(fontSize: 18),
         ),
 
         SizedBox(height: 20),
 
-        // Affichage des options de réponses
         // Affichage des options de réponses
         ...quizzes[0].questions[currentQuestionIndex].options.map<Widget>((option) {
           return Padding(
@@ -51,7 +86,9 @@ class QuizWidget extends StatelessWidget {
               width: double.infinity, // Prend toute la largeur
               child: TextButton(
                 style: TextButton.styleFrom(
-                  backgroundColor: selectedOption == option ? ColorConstants.accentuationColor : ColorConstants.primaryColor,
+                  backgroundColor: selectedOption == option
+                      ? ColorConstants.accentuationColor
+                      : ColorConstants.primaryColor,
                   padding: EdgeInsets.symmetric(vertical: 15.0), // Padding vertical du bouton
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10.0), // Bordures arrondies
@@ -77,6 +114,7 @@ class QuizWidget extends StatelessWidget {
 
         CustomButton(
           text: 'Valider la réponse',
+          textColor: Colors.white,
           onPressed: () {
             if (!selectedOption.isNotEmpty) {
               ScaffoldMessenger.of(context).showSnackBar(
@@ -91,4 +129,3 @@ class QuizWidget extends StatelessWidget {
     );
   }
 }
-
